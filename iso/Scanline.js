b7b06756bc13af90bb8171cacd33d6ee02f9b391
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2016-07-22 01:43:35 +0300

    DJU. I will remove it soon, its not needed actually. DERP.

--- iso/Scanline.js
+++ iso/Scanline.js
@@ -1,16 +1,34 @@
 var PIXI = require("pixi.js");
 
 function Scanline() {
-    //only pooled stuff here
+    //only pooled stuff here is allowed
     this.segments = [];
+    this.heap = new EventHeap();
+    this.tree = new List();
+    this.queue = [];
+    this.eventPool = [];
 };
 
 var EPS = 1e-5;
 
 Scanline.prototype = {
-    process(childs) {
-        this._init(childs);
+    /**
+     * accepts array of children, each of them MUST have a "segment" component
+     *
+     * @param childs
+     */
+    process(sortChildren) {
+        this._init(sortChildren);
         this._clean();
+    },
+
+    _eventCreate(x, type, seg) {
+        return this.eventPool.pop() || new Event(x, type, seg);
+    },
+
+    _eventDestroy(event) {
+        event.clear();
+        this.eventPool.push(event);
     },
 
     _init(childs) {
@@ -20,7 +38,46 @@ Scanline.prototype = {
             seg.owner = childs[i];
             seg.num = i;
             segments.push(seg);
+            //open
+            this.heap.push(this._eventCreate(seg.x1, 2, seg));
+            //close
+            this.heap.push(this._eventCreate(seg.x2, 0, seg));
         }
+    },
+
+    _scanLine() {
+        var heap = this.heap;
+        var list = this.list;
+        while (!heap.isEmpty()) {
+            var event = heap.pop();
+            var seg = event.seg;
+            if (event.type === 2) {
+                //insert new segment
+                var lb = list.lowerBound(seg);
+                if (lb !== null) {
+                    var x = lb.intersect(seg);
+                    if (x !== null) {
+                        this.heap.push(this._eventCreate(x, 1, lb));
+                        seg.djoin(lb);
+                    }
+                    lb.nextEdges.push(seg);
+                }
+                list.insertAfter(lb, seg);
+            } else if (event.type === 1) {
+                //TODO: may be create new segment for that thing, for better solution
+                list.moveUp(seg);
+                //move segment up
+            } else if (event.type === 0) {
+                list.remove(seg);
+            }
+        }
+    },
+
+    _topSort() {
+        var queue = this.queue;
+
+
+        queue.length = 0;
     },
 
     _clean() {
@@ -34,6 +91,7 @@ Scanline.prototype = {
 
 /**
  * segment, x1 is always less than x2 in it
+ *
  * @class
  * @param x1
  * @param y1
@@ -53,18 +111,21 @@ function Segment(x1, y1, x2, y2) {
 
     /**
      * temporary, assigned by Scanline
+     *
      * @type {null}
      */
     this.owner = null;
 
     /**
      * temp number, update order
+     *
      * @type {number}
      */
     this.num = 0;
 
     /**
      * second stage - next segment in the list
+     *
      * @type {null}
      */
     this.next = null;
@@ -72,23 +133,36 @@ function Segment(x1, y1, x2, y2) {
 
     /**
      * result of the second stage
+     *
      * @type {Array}
      */
     this.nextEdges = [];
 
     /**
      * inbound counter exists for final process of sorting
+     *
      * @type {number}
      */
     this.inboundCounter = 0;
+
+    /**
+     * loops counter
+     * @type {number}
+     */
+    this.loopsCounter = 0;
 };
 
 Segment.prototype = {
     clear: function () {
         this.nextEdges.length = 0;
         this.inboundCounter = 0;
+        this.loopsCounter = 0;
         this.next = null;
+        this.prev = null;
+        this.prev = null;
         this.owner = null;
+        this.djuParent = this;
+        this.djuRank = 0;
     },
 
     update: function (x1, y1, x2, y2) {
@@ -110,9 +184,10 @@ Segment.prototype = {
 
     /**
      * intersects two horizontal segments
+     *
      * @param seg2 {Segment} another segment
      */
-    intersectDiagonal: function (seg2) {
+    intersect: function (seg2) {
         if (this._k !== seg2._k) {
             return false;
         }
@@ -126,6 +201,7 @@ Segment.prototype = {
 
     /**
      * checks if its above or below of seg2 when seg2 is starting
+     *
      * @param seg2
      */
     below: function (seg2) {
@@ -134,12 +210,90 @@ Segment.prototype = {
     },
 
     /**
-     * TODO
+     * we dont need that for first version
+     *
      * @param seg {Segment}
      * @returns {boolean}
      */
     intersectVertical: function (seg) {
         return false;
+    },
+
+    addEdge: function(otherSegment) {
+        var p1 = this.djup(), p2 = otherSegment.djup();
+        if (p1 !== p2) {
+            p2.inboundCounter++;
+            p1.nextEdges.push(p2);
+        }
+    },
+
+    /**
+     * called only from djoin
+     * @param otherSegment
+     */
+    addEdges: function (otherSegment) {
+        var i, n;
+
+        n = this.nextEdges;
+        for (i=0; i<n;i++) {
+            while (this.nextEdges[i] && this.nextEdges[i].djup() === this) {
+                if (otherSegment.nextEdges.length > 0) {
+                    this.nextEdges[i] = otherSegment.nextEdges.pop();
+                } else {
+                    this.nextEdges[i] = null;
+                    this.loopsCounter++;
+                }
+            }
+        }
+
+        n = otherSegment.nextEdges;
+        for (i = 0; i < n; i++) {
+            var p = otherSegment.nextEdges[i];
+            if (p) {
+                p = p.djup();
+                if (p !== this) {
+                    this.nextEdges.push(p);
+                    this.inboundCounter++;
+                }
+            }
+        }
+    },
+
+    djup: function () {
+        var x = this;
+        while (x !== x.djuParent) {
+            x = x.djuParent;
+        }
+        var y = this;
+        while (y.djuParent !== x) {
+            var z = y.djuParent;
+            y.djuParent = x;
+            y = z;
+        }
+        return this.djuParent;
+    },
+    /**
+     * together forever^W for this calculation
+     *
+     * @param seg
+     */
+    djoin: function (seg) {
+        var p1 = this.djup(), p2 = seg.djup();
+        if (p1 === p2) {
+            return;
+        }
+        //TODO: add stuff with less edges to stuff with more edges
+        if (p1.djuRank < p2.djuRank) {
+            p1.djuParent = p2;
+            p2.addEdges(p1);
+        } else if (p1.djuRank > p2.djuRank) {
+            p2.djuParent = p1;
+            p1.addEdges(p2);
+        } else {
+            p1.djuParent = p2;
+            p2.djuRank++;
+            p2.addEdges(p1);
+        }
     }
 };
 
@@ -154,20 +308,70 @@ function List() {
 }
 
 List.prototype = {
-    insert: function (where, elem) {
+    lowerBound: function (elem) {
+        var t = this.head;
+        var ans = null;
+        if (t !== null && t.below(elem)) {
+            ans = t;
+            t = t.next;
+        }
+        return ans;
+    },
+    insertAfter: function (where, elem) {
         if (where === null) {
             elem.next = this.head;
-            this.head = elem;
+            elem.prev = null;
         } else {
             elem.next = where.next;
-            where.next = elem;
+            elem.prev = where;
         }
+        if (elem.next) {
+            elem.next.prev = elem;
+            elem.addEdge(elem.next);
+        } else {
+            this.tail = elem;
+        }
+        if (elem.prev) {
+            elem.prev.next = elem;
+            elem.prev.addEdge(elem);
+        } else {
+            this.head = elem;
+        }
+    },
+    remove: function (elem) {
+        if (elem.next) {
+            elem.next.prev = elem.prev;
+        } else {
+            this.tail = elem.prev;
+        }
+        if (elem.prev) {
+            elem.prev.next = elem.next;
+            elem.prev.nextEdges.push(elem.next);
+        } else {
+            this.head = elem.next;
+        }
+    },
+    moveUp: function (elem) {
+        var next = elem.next;
+        if (!next) {
+            return false;
+        }
+        this.remove(elem);
+        this.insertAfter(next, elem);
+        return true;
     }
 };
 
-function Event(type, x) {
+function Event(type, x, seg) {
     this.type = type;
     this.x = x;
+    this.seg = seg;
+};
+
+Event.prototype = {
+    clear: function () {
+        this.seg = null;
+    }
 };
 
 /**
@@ -179,7 +383,7 @@ function EventHeap() {
 };
 
 EventHeap.prototype = {
-    push(event) {
+    push: function (event) {
         var arr = this.arr;
         var num = arr.length;
         arr.push(event);
@@ -195,7 +399,14 @@ EventHeap.prototype = {
         arr[num] = event;
     },
 
-    pop() {
+    isEmpty: function () {
+        return this.arr.length > 1;
+    },
+
+    pop: function () {
+        if (this.arr.length === 1) {
+            return null;
+        }
         var arr = this.arr;
         var res = arr[1];
         arr[1] = arr[arr.length - 1];
